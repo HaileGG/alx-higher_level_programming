29cc6610b30492d22888601d63e871528a224a42
Haile Gebresilassie <hailye2013@gmail.com>
2022-05-30 10:37:56 -0700

    My Completed task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-str = "Python is an interpreted, interactive, object-oriented programming\ 
-language that combines remarkable power with very clear syntax"
-str = str[39:66] + [106:112] + [:6]
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = str[39:67] + [107:112] + [0:6]
 print(str)
