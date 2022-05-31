897a8b9ef7270762ba4092b22bdf919e4825acb9
Haile Gebresilassie <hailye2013@gmail.com>
2022-05-31 05:30:03 -0700

    My Completed Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,7 +9,7 @@
  *
  * Return: pointer to the new node
  */
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new;
 	listint_t *curt;
