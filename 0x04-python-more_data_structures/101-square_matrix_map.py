95cb79a41b4e73d87d5d3b6f3a1b2d4319adf62b
Haile Gebresilassie <hailye2013@gmail.com>
2022-06-08 20:04:08 +0300

    My Completed Task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def spaure_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
