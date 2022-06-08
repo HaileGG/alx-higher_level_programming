1dd9848fc836a6d4df8385ab152adc99d2e8ab3d
Haile Gebresilassie <hailye2013@gmail.com>
2022-06-08 20:13:43 +0300

    My Completed Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for key, value in a_dixtionary.items():
-            if valuse is max(a_dictionary.values()):
-                return key
-    else:
-        return None
+        return (max(a_dictionary.keys()))
