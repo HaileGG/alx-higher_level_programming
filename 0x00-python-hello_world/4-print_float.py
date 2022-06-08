e01267e3df895e671afe4d91a0cc88ca1b04af08
Haile Gebresilassie <hailye2013@gmail.com>
2022-06-08 20:25:00 +0300

    My Completed Task 4

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-<<<<<<< HEAD
-print("Float: {:0.2f}".format(number))
-=======
-print(F"{number:.2f} Float: ")
->>>>>>> 0d93f069b05f36959f5bf2bb8edb7a0ff19b12e4
+print("Float: {:.2f}".format(number))
