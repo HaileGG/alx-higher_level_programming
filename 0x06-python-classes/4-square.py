eddb072ead884e7e06e311eb5671aaa355978af0
Haile Gebresilassie <hailye2013@gmail.com>
2022-06-21 20:15:24 +0300

    My Completed Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and public instance method"""
+
+
 class Square:
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
+
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        return self.__size
+        return(self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("Size must be an integer")
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        ar = self.__size * self__size
-        return ar
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
