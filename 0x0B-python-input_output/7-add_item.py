74b980b6a265cc7e10db377d8177b7f7574dec5f
Haile Gebresilassie <hailye2013@gmail.com>
2022-07-05 21:11:56 +0300

    My Completed Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""Add item script."""
+"""
+    6-from_json_string.py
+    Function that writes an Object to \
+    a text file, using a JSON representation.
+"""
 import sys
+import json
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+def create_file_list():
+    """Adds all arguments to a Python list, and then save them to a file"""
+    obj_9 = load_from_json_file("add_item.json")
 
-argc = len(sys.argv)
+    for i in range(1, len(sys.argv)):
+        obj_9.append(str(sys.argv[i]))
 
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
+    save_to_json_file(obj_9, "add_item.json")
 
-save_to_json_file(lst, "add_item.json")
+if len(sys.argv) < 2:
+    try:
+        f = open("add_item.json")
+        f.close()
+    except IOError:
+        save_to_json_file([], "add_item.json")
+else:
+    try:
+        f = open("add_item.json")
+        f.close()
+        create_file_list()
+    except IOError:
+        save_to_json_file([], "add_item.json")
+        create_file_list()
