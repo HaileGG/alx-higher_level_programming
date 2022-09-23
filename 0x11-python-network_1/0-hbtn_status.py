fd9a420fe50c7396d2d95cd76b77abd786a4bf4a
Haile Gebresilassie <hailye2013@gmail.com>
2022-09-23 20:41:11 +0300

    My Completed Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://alx-intranet.hbtn.io/status
-"""
-
-from urllib.request import Request, urlopen
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request('https://alx-intranet.hbtn.io/status')
+    """fetches https://alx-intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
 
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
