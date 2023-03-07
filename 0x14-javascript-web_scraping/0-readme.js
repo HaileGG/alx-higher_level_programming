0c8f61daba1a3066663defd3237756bc57cc01c8
Haile gebresilassie <hailye2013@gmail.com>
2023-03-07 16:22:25 +0300

    My Completed task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file
-
 const fs = require('fs');
-const filepath = process.argv[2];
 
-fs.readFile(filepath, 'utf-8', (err, contents) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(contents);
+    console.log(data.toString());
   }
 });
